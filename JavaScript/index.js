053978245d76244edf293de4fb6bc7c2972f8098
Cahil Cowan <kevinc20000720@gmail.com>
2025-11-14 19:54:07 -0500

    Add getComputerChoice()
    To randomly generate an integer from 1 to 3 and
    use one of those values to return rock, paper or
    scissors.

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+// Get the computer choice
+function getComputerChoice ()
+{
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch (computerChoice)
+    {
+        case 1:
+            return "rock";
+        case 2: 
+            return "paper";
+        case 3:
+            return "scissor";
+    }
+}
