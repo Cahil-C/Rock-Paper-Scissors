e2330e616e9220a0693c2fbea76b73902dc0d89f
Cahil Cowan <kevinc20000720@gmail.com>
2025-11-14 22:51:49 -0500

    Fix bug: Change computerScore to computerChoice
    Inside the playRound(), the output for in the
    block of some conditionals was computerScore,
    fix it to be "computer" instead of number.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -23,60 +23,70 @@ function getHumanChoice ()
     return humanChoice
 }
 
-// Keep track of players score (computer and the human)
-let computerScore = 0;
-let humanScore = 0;
-
-// Single Round
-function playRound (humanChoice, computerChoice)
+function playGame ()
 {
-    if (computerChoice === "rock" && humanChoice === "rock")
-    {
-        console.log(`It's a tie! ${computerScore} and ${humanChoice}`);
-        computerScore += 1;
-        humanScore += 1; 
-    }
-    else if (computerChoice === "rock" && humanChoice === "paper")
-    {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1; 
-    }
-    else if (computerChoice === "rock" && humanChoice === "scissor")
-    {
-        console.log(`You lose! ${computerScore} beats ${humanChoice}`);
-        computerScore += 1;
-    }
-    else if (computerChoice === "paper" && humanChoice === "rock")
-    {
-        console.log(`You lose! ${computerScore} beats ${humanChoice}`);
-        computerScore += 1;
-    }
-    else if (computerChoice === "paper" && humanChoice === "paper")
-    {
-        console.log(`It's a tie ${humanChoice} and ${computerChoice}`);
-        computerScore += 1;
-        humanScore += 1;
-    }
-    else if (computerChoice === "paper" && humanChoice === "scissor")
-    {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    }
-    else if (computerChoice === "scissor" && humanChoice === "rock")
-    {
-        console.log(`You win ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    }
-    else if (computerChoice === "scissor" && humanChoice === "paper")
-    {
-        console.log(`You lose ${computerChoice} and ${humanChoice}`);
-        computerScore += 1;
-    }
-    else if (computerChoice === "scissor" && humanChoice === "scissor")
-    {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        computerScore += 1;
-        humanChoice += 1;
-    }
+    console.log("Let's play rock, paper, scissor")
 
+    // Keep track of players score (computer and the human)
+    let computerScore = 0;
+    let humanScore = 0;
+
+    // Single Round
+    function playRound (humanChoice, computerChoice)
+    {
+
+        if (computerChoice === "rock" && humanChoice === "rock")
+        {
+            console.log(`It's a tie! ${computerChoice} and ${humanChoice}`);
+            computerScore += 1;
+            humanScore += 1; 
+        }
+        else if (computerChoice === "rock" && humanChoice === "paper")
+        {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1; 
+        }
+        else if (computerChoice === "rock" && humanChoice === "scissor")
+        {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if (computerChoice === "paper" && humanChoice === "rock")
+        {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if (computerChoice === "paper" && humanChoice === "paper")
+        {
+            console.log(`It's a tie ${humanChoice} and ${computerChoice}`);
+            computerScore += 1;
+            humanScore += 1;
+        }
+        else if (computerChoice === "paper" && humanChoice === "scissor")
+        {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+        }
+        else if (computerChoice === "scissor" && humanChoice === "rock")
+        {
+            console.log(`You win ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+        }
+        else if (computerChoice === "scissor" && humanChoice === "paper")
+        {
+            console.log(`You lose ${computerChoice} and ${humanChoice}`);
+            computerScore += 1;
+        }
+        else if (computerChoice === "scissor" && humanChoice === "scissor")
+        {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            computerScore += 1;
+            humanChoice += 1;
+        }
+    } 
+
+    playRound("rock", "paper");
 }
+
+// 
+playGame()
